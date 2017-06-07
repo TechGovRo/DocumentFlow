62840c6043da5d798e85bba0b49f4992cf003c2a
Mihai Ciucu <mihai@infra.org>
2017-06-07 12:13:22 +0300

    * install dev env script

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -1,1 +1,17 @@
+#!/usr/bin/env bash
+
 apt update
+
+apt install -y build-essential libssl-dev
+apt install -y curl postgresql redis-server g++
+apt install -y postgresql-server-dev-all
+
+# Install NodeJS 8
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt install -y nodejs
+
+npm install -g babel-cli rollup
+npm install --dev
+
+apt install -y libjpeg-dev libpng-dev
+apt install -y python3-dev
